892adfa576882002c09c56a61392a03ae39c1bec
aoverb <250614271@qq.com>
2026-01-28 20:40:37 +0800

    libc引用sysroot构建

---
+++ build_libc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export SYSROOT=`pwd`/SYSROOT
+echo $SYSROOT
+mkdir -p $SYSROOT/usr/lib
+mkdir -p $SYSROOT/usr/include
+cd kernel
+make install-headers
+cd ..
+cd libc
+make install-headers clean all
+cd ..
